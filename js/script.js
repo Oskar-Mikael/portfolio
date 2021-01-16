2a2ae4b9ed6f7f1cce09a8e5d41377c55805de8b
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-01-16 17:50:43 +0100

    Remove unused functions and add additional js for hamburger animation

--- js/script.js
+++ js/script.js
@@ -3,12 +3,5 @@ const navul = document.querySelector("#menu");
 
 hamburger.addEventListener("click", () => {
     navul.classList.toggle("show");
-})
-
-function showText(text) {
-    document.querySelector(".project-img").innerHTML=text;
-}
-
-function hideText() {
-    document.querySelector("project-img").innerHTML="";
-}
+    hamburger.classList.toggle('open')
+});
