ad611924af24f9f753899c0c67d075976fe58fae
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-02 15:18:35 +0100

    Replace functions with arrow functions

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const hamburger = $('#hamburger');
 const navul = $('#menu');
 
 //Function for toggling nav menu in mobile view
-hamburger.on('click', function() {
+hamburger.on('click', () => {
     navul.toggleClass("show")
     hamburger.toggleClass('open')
 })
 
 //Function for button to scroll down to projects
-$('.header-button').on('click', function() {
+$('.header-button').on('click', () => {
     $([document.documentElement, document.body]).animate({
         scrollTop: $('#projects').offset().top
     }, 800)
 })
 
-window.onscroll = function() {displayScrollButton()};
+window.onscroll = () => {displayScrollButton()};
 
 //Function for hiding and showing scroll-top button
 function displayScrollButton() {
@@ -26,16 +26,16 @@ function displayScrollButton() {
 }
 
 //Function for button to scroll to top
-$('#scroll-top').on('click', function() {
+$('#scroll-top').on('click', () => {
     $([document.documentElement, document.body]).animate({
         scrollTop: $('#top').offset().top
     }, 800)
 })
 
 //Function for toggling CV dropdown menu
-$('.dropdown').hover(function() {
+$('.dropdown').hover( () => {
     $('.dropdown-content').hide().slideToggle(300);
-}, function() {
+}, () => {
     $('.dropdown-content').show().slideToggle(300);
 })
 
