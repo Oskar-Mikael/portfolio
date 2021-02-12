6f6896f7a7add3fb15c6096ce1a1e80f67f6b48f
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-13 00:12:48 +0100

    Add rotation to dropdown arrow

--- js/script.js
+++ js/script.js
@@ -37,15 +37,18 @@ $('.dropdown').on('click', () => {
     if ($('.dropdown-content').css('display') == 'none') {
 
         $('.dropdown-content').hide().slideToggle(300);
+        $('.dropdown-arrow').css('transform', 'rotate(180deg)');
 
     } else {
 
         $('.dropdown-content').show().slideToggle(300);
+        $('.dropdown-arrow').css('transform', 'rotate(0deg)');
     }
 })
 
 $(document).click( () => {
     $('.dropdown-content').hide().slideUp(300);
+    $('.dropdown-arrow').css('transform', 'rotate(0deg)');
 })
 
 $('.dropdown').click( (e) => {
